36b2fd56bf08228a4cb2a354063919d8ecb1dff7
Gabrielli Borba <gabrielliborba42@gmail.com>
2026-09-18 09:14:32 -0300

    feat: setup inicial do backend com Spring Boot 4 e Spring AI (Groq)

---
+++ backend/src/main/java/com/gabrielli/resume_ai_optimizer/ResumeAiOptimizerApplication.java
@@ -0,0 +1,14 @@
+package com.gabrielli.resume_ai_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ResumeAiOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAiOptimizerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrielli/resume_ai_optimizer/ResumeAiOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielli.resume_ai_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAiOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
